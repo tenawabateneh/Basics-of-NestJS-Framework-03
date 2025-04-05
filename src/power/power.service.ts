b4f0b48d91a6f3caa5f76eea5e77c3fac79a0589
tenaw <tenawabateneh06@gmail.com>
2025-04-05 17:41:01 +0300

    Setting Up DI Between Modules(Powre & Cpu Modules).

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PowerService {}
+export class PowerService {
+  supplayPower(watts: string) {
+    console.log(`Supplying ${watts} worth of power`)
+  }
+}
