7694a81960e4a3eae3f385f83746184991468c71
tenaw <tenawabateneh06@gmail.com>
2025-04-05 17:58:07 +0300

    Setting Up DI Between Modules(Powre & Disk Modules).

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) { }
+
+  ComputerModule(a: number, b: number) {
+    console.log("Drawing 20 watts of power from Power Service")
+    this.powerService.supplayPower(20)
+    return a + b
+  }
 }
--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PowerService {
-  supplayPower(watts: string) {
+  supplayPower(watts: number) {
     console.log(`Supplying ${watts} worth of power`)
   }
 }
